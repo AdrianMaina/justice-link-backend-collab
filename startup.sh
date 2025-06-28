9f8c6029a43b3b9a6caa3b90391f899059e65694
Haron M Maina <adrian.maina@student.moringaschool.com>
2025-06-29 00:40:42 +0300

    fix: set FLASK_APP in startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Set the FLASK_APP environment variable so Flask knows where to find the app
+export FLASK_APP=run.py
+
 # Apply database migrations
 echo "Applying database migrations..."
 flask db upgrade
